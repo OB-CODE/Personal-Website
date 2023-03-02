ecf64bb1550be048885f34f98c0ba95d2ffc93b4
mitchell <mitchell.s.obrien@gmail.com>
2023-03-02 18:46:26 +1100

    remove library that is not working with modern react for landing text

--- src/components/landing/LandingHeader.js
+++ src/components/landing/LandingHeader.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./Landing.scss";
-import Typical from 'react-typical'
+
 
 
 const LandingHeader = () => {
@@ -9,22 +9,12 @@ const LandingHeader = () => {
       <div className="strip"></div>
 
       <div className='Container'>
-        <div className="heading">
-          <h5>
-            <Typical
-            steps={['Mitchell', 1000, `Mitch O'Brien`, 1000]}
-            loop={2}
-            wrapper="div"
-              // <h5>Mitch</h5>
-              // <h5>O'Brien</h5>
-            />
-          </h5>
+        <div className="landHeading"> 
+          <h4>Mitch</h4>
+          <h5 className='second'>O'Brien.</h5>
         </div>
         <h1 className='jobs'>
-        <Typical
-          steps={[`<01101000 01101001 />`, 400, `<Software Engineer />`, 1000]}
-          loop={1}
-         />
+        {`<Software Engineer />`}
          </h1>
       </div>
     </div>
