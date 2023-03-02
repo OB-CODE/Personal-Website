e1f1d5a6483f7c470e8586d009e6172d3becc692
mitchell <mitchell.s.obrien@gmail.com>
2023-03-02 13:35:44 +1100

    animation added to landing text

--- src/components/landing/LandingHeader.js
+++ src/components/landing/LandingHeader.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import "./Landing.scss";
+import Typical from 'react-typical'
+
 
 const LandingHeader = () => {
   return (
@@ -8,10 +10,22 @@ const LandingHeader = () => {
 
       <div className='Container'>
         <div className="heading">
-          <h5>Mitch</h5>
-          <h5>O'Brien</h5>
+          <h5>
+            <Typical
+            steps={['Mitchell', 1000, `Mitch O'Brien`, 1000]}
+            loop={2}
+            wrapper="div"
+              // <h5>Mitch</h5>
+              // <h5>O'Brien</h5>
+            />
+          </h5>
         </div>
-        <h1 className='jobs'>{`<Software Engineer />`}</h1>
+        <h1 className='jobs'>
+        <Typical
+          steps={[`<01101000 01101001 />`, 400, `<Software Engineer />`, 1000]}
+          loop={1}
+         />
+         </h1>
       </div>
     </div>
   
