5ddd584622482898c118419eb43d7db69233d4d9
mitchell <mitchell.s.obrien@gmail.com>
2023-03-02 16:41:22 +1100

    CSS on project images started

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -9,12 +9,14 @@ import poke1 from './assets/poke1.png'
 import poke2 from './assets/poke2.png'
 import HomeH1 from './assets/HomeH1.png'
 import HomeH2 from './assets/HomeH2.png'
-import { Link } from '@mui/material'
+// import { Link } from '@mui/material'
+import 'react-slideshow-image/dist/styles.css'
+import { Slide } from 'react-slideshow-image';
 
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
+// import Box from '@mui/material/Box';
+// import Button from '@mui/material/Button';
+// import Typography from '@mui/material/Typography';
+// import Modal from '@mui/material/Modal';
 
 
 const Projects = () => {
@@ -22,32 +24,28 @@ const Projects = () => {
 const allProjects = [
       {
       name: 'Home Harvest (Final Project)',
-      image1: HomeH1,
-      image2: HomeH2,
+      images: [HomeH1, HomeH2],
       gitHub: "https://github.com/OB-CODE/Home-Harvest",
       url: 'https://mitchell-home-harvest.surge.sh/',
       description: [`About:`, `Home Harvest is designed to reduce the barriers of entry for the everyday person trying to grow produce.`, `3 API's intergrated and a personal database.`, `Renders 12 months of weather data in table and graph format.`, `Calculates ideal planting times based off growing conditions for each crop.` ]
       },
       {
       name: 'Pokemon EXPRESS game',
-      image1: poke1,
-      image2: poke2,
+      images: [poke1, poke2],
       gitHub: "https://github.com/OB-CODE/pokemon-game",
       url: 'https://github.com/OB-CODE/pokemon-game/blob/main',
       description: [`Requirements:`, `Craft thoughtful user stories together, as a team`, `Manage team contributions and collaboration using a standard Git flow on Github`, `Layout and style your front-end with clean & well-formatted CSS`, `Include separate HTML / CSS / JavaScript files`, `GOAL: Work with other classmates to architect, design, and collaboratively build a product.`]
       },
       {
       name: 'Surf Spot - Currently down (Need to change hositng from Fly.io)',
-      image1: surf1,
-      image2: surf2,
+      images: [surf1, surf2],
       gitHub: "https://github.com/OB-CODE/Surf-spot-SEI-Project-2",
       url: '',
       description: [`Requirements:`, `Have at least 2 resources(tables) (more if they make sense) – one representing someone using your application, and one that represents the main functional idea for your app`, `Include sign up/log in functionality(if they make sense), with encrypted passwords & an authorization flow`, `Have complete RESTful routes for at least one of your resources with GET, POST, PATCH, and DELETE`, `DESCRIPTION: Surf Spot allows full CRUD features and is a full stack application build using Ruby. It intergrates with a custome backend API and global weather data.` ]
       },
       {
       name: 'Tic-Tac-Toe',
-      image1: tic1,
-      image2: tic2,
+      images: [tic1, tic2],
       gitHub: "https://github.com/OB-CODE/Tic-Tac-Toe",
       url: 'https://ob-code.github.io/Tic-Tac-Toe/',
       description: [`Requirements:`, `Render a game in the browser`, `Switch turns between more than one player`, `Design logic for winning & visually display which player won`, `Include separate HTML / CSS / JavaScript files`, `Use Javascript for DOM manipulation`]
@@ -55,21 +53,22 @@ const allProjects = [
 
 ]
 
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-};
+// for modal - not is use atm.
+// const style = {
+//   position: 'absolute',
+//   top: '50%',
+//   left: '50%',
+//   transform: 'translate(-50%, -50%)',
+//   width: 400,
+//   bgcolor: 'background.paper',
+//   border: '2px solid #000',
+//   boxShadow: 24,
+//   p: 4,
+// };
 
-const [open, setOpen] = React.useState(false);
-const handleOpen = () => setOpen(true);
-const handleClose = () => setOpen(false);
+// const [open, setOpen] = React.useState(false);
+// const handleOpen = () => setOpen(true);
+// const handleClose = () => setOpen(false);
 
   return (
   <section id="projects">
@@ -111,11 +110,22 @@ const handleClose = () => setOpen(false);
                 </div>
               </div>
 
-              <div  onClick={handleOpen} className="rightContent">
-                <div className='images'><img src={project.image1} alt="" /></div>
-                <div className='images'><img src={project.image2} alt="" /></div>
+              <div  
+              // onClick={handleOpen} 
+              className="rightContent">
+                <h2>PROJECT IMAGES</h2>
+                <Slide>
+                  {project.images.map((image, index) =>
+                  <div key={index}
+                  className="images">
+                    <div className='containProjectImages'>
+                      <img src={image} alt="" />
+                    </div>
+                  </div>
+                  )}
+                </Slide>
               </div>
-              <Modal
+              {/* <Modal
                 open={open}
                 onClose={handleClose}
                 aria-labelledby="modal-modal-title"
@@ -135,7 +145,7 @@ const handleClose = () => setOpen(false);
                     <p>Have fun!</p>
                   </Typography>
                 </Box>
-              </Modal>
+              </Modal> */}
             </div>   
           </div>
           )}
