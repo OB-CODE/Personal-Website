cc8c6872739c0db63a2199c627fcb3bab5643498
Mitchell O'Brien <115781530+OB-CODE@users.noreply.github.com>
2025-04-19 17:06:28 +1000

    Update linkDetails.js
    Removed unused link.

--- src/components/Nav/linkDetails.js
+++ src/components/Nav/linkDetails.js
@@ -5,7 +5,6 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 const actions = [
   { icon: <EmailIcon />, name: 'Gmail', url: 'https://mail.google.com/' },
-  { icon: <TwitterIcon />, name: 'Twitter', url: 'https://twitter.com/Mitch_OB_CODE' },
   { icon: <GitHubIcon />, name: 'GitHub', url: 'https://github.com/OB-CODE' },
   { icon: <LinkedInIcon />, name: 'LinkedIn', url: 'https://linkedin.com/in/mitchellobriense01' },
 ]
